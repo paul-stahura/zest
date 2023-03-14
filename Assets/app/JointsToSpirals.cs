8bd3c9e75c81f25d71a0efd44928c3c48ce5e0a8
Chris Cowherd <madcow@gmail.com>
2023-03-14 16:40:17 -0700

    Spiral centers drawn with crosshairs 4 better viz

--- Assets/app/JointsToSpirals.cs
+++ Assets/app/JointsToSpirals.cs
@@ -86,152 +86,21 @@ public class JointsToSpirals : MonoBehaviour
 
     void drawCenterPoints(Camera cam, Zeta.Spiral spiral)
     {
-        var SCALAR = 400f;
-        // var p = new Vector3[spiral.middleIndex];
-        // for (var i = 0; i < spiral.middleIndex; i++)
-        // {
-        //     var s = spiral.spirals[i];
-        //     p[i] = new Vector3(s.x, s.y, 0);
-        // }
-        // MeshUtils.ChildrenFromPoints(transform, "Points", p, Material, Vector3.one);
         dotColor.a = dotTransparency.value;
         Draw.Color = dotColor;
-
-        var index = Zeta.ImagToIndex(spiral.input.Imaginary);
+        Draw.Thickness = 1;
 
         var start = 0;
         if (showJust2.isOn)
-        {
             start = spiral.middleIndex - 1;
-        }
 
-        var size = cam.orthographicSize / SCALAR;
-        var rc = new Rect(-size, -size, size * 2, size * 2);
         for (var i = start; i < spiral.spirals.Length; i++)
         {
-            var from = spiral.spirals[i];
-            Draw.Rectangle(from, rc);
-
-            // I was going to draw a line straight down from the dot to the link
-            // but its not finished here. This is to diagnose the wiggling dot.
-            //
-            // var l = (int)spiral.SpiralMiddleIndex(index, i);
-            // var link = spiral.joints[l+1] - spiral.joints[l];
-            // link.Dot(from);
+            var pt = spiral.spirals[i];
+            Draw.Ring(pt, .001f);
+            ShapesUtils.DrawCross(pt, .002f, .5f);
         }
     }
-
-    void drawTrail(Zeta.Spiral spiral)
-    {
-        if (trailTransparency.value == 0)
-        {
-            return;
-        }
-
-        // for (var i = 0; i < spiral.spirals.Length; i++)
-        // {
-        //     var s = spiral.spirals[i];
-        //     if (trail.Contains(s))
-        //         continue;
-
-        //     trail.Add(new Vector3(s.x, s.y, 0));
-        // }
-
-        var spiralNumber = 2;
-
-        var mi = Zeta.ImagToIndex(app.Imag);
-        var i = (int)spiral.SpiralMiddleIndex(mi, spiralNumber);
-
-        // Highlight the spiral middle link
-        var j1 = spiral.joints[i];
-        var j2 = spiral.joints[i + 1];
-
-        var s = spiral.spirals[spiralNumber];
-        var pt = new Vector(s.x, s.y);
-
-        trailParent.transform.localPosition = j1.ToVector3();
-
-
-        // Offset vector from the middle link first point
-        var p = (pt - j1).ToVector3();
-
-        //
-        // TODO:
-        // You need the angle of the line between these two lines:
-        // j1 and p
-        // j1 and j2
-        // then apply that rotation to trail transform to move all the previous mesh points
-        // then add the latest point
-        // 
-        var temp = (j2 - j1).ToVector3();
-        var angle = Mathf.Atan2(temp.y, temp.x) * Mathf.Rad2Deg;
-
-        // var rot = Quaternion.AngleAxis(180 - angle, Vector3.forward);
-
-        // https://forum.unity.com/threads/rotate-vector-by-quaternion.21687/
-        // p = Quaternion.Inverse(rot) * p;
-        // if (trail.Count == 0)
-
-        if (!trail.Contains(p))
-            trail.Add(p);
-
-        while (trail.Count > 65535)
-            trail.RemoveAt(0);
-
-        // trailParent.transform.rotation = rot;
-
-    }
-
-    public GameObject trailParent;
-
-    // void calculateTrail(Zeta.Spiral spiral)
-    // {
-    //     using (Draw.StyleScope)
-    //     {
-    //         Draw.Color = Color.magenta;
-    //         Draw.Thickness = 4;
-
-    //         var mi = spiral.middleIndex - 1;
-
-    //         var count = 0;
-    //         var start = new Vector2();
-
-    //         for (var imag = Zeta.IndexToImag(mi); imag < Zeta.IndexToImag(mi + 1); imag += .05)
-    //         {
-    //             var spiral = new Zeta.Spiral(new System.Numerics.Complex(app.Real, imag), false);
-
-
-    //             var pt = spiral.zeta.ToVector();
-    //             var slope = -pt.x / pt.y;
-
-    //             var z2 = (pt / 2).ToVector2(); // zeta over 2
-    //             var zeta = pt.ToVector2();
-
-    //             var from = spiral.links[spiral.middleIndex].ToVector2();
-    //             var norm = (z2).normalized;
-    //             var dot = Vector2.Dot(from, norm);
-    //             var to = zeta + from - 2 * dot * norm; // reflect from about a normal (z2)
-
-    //             // if (count == 0)
-    //             // {
-    //             //     start = to;
-    //             //     count++;
-    //             //     continue;
-    //             // }
-
-    //             // Draw.Line(start, to);
-    //             // start = to;
-    //             // count++;
-
-    //             if (count == 3)
-    //                 break;
-
-    //             count++;
-    //             ShapesUtils.DrawCross(to);
-    //             // break;
-    //         }
-    //     }
-    // }
 }
 
 
--- Assets/utils/TransformUtils.cs
+++ Assets/utils/TransformUtils.cs
@@ -15,4 +15,30 @@ public static class TransformUtils
         // the parent game object.  This seems to do the trick:
         // Resources.UnloadUnusedAssets();
     }
+
+    /// <summary>
+    /// Creates a transform from the difference between this one and another.
+    /// </summary>
+    /// <param name="transform"></param>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public static Vector3 Difference(this Transform transform, Transform other)
+    {
+        var pos = other.position - transform.position;
+        var rot = other.rotation * Quaternion.Inverse(transform.rotation);
+
+        return pos;
+    }
+
+    /// <summary>
+    /// Adds another transform's position and rotation to this one.
+    /// Use it to apply a calculated difference.
+    /// </summary>
+    /// <param name="transform"></param>
+    /// <param name="other"></param>
+    public static void Apply(this Transform transform, Transform other)
+    {
+        transform.position += other.position;
+        transform.rotation = other.rotation * transform.rotation;
+    }
 }
