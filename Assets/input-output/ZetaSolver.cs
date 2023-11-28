0cfd82f2b3902cb50e9b562cfb87f5d22f18a81e
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2023-11-28 01:06:24 -0800

    Calculating Zeta with Job System
    TODO
    - Draw is not multithread safe :angry:

--- Assets/input-output/ZetaSolver.cs
+++ Assets/input-output/ZetaSolver.cs
@@ -4,6 +4,12 @@ using System.Collections.Generic;
 using Shapes;
 using Complex = System.Numerics.Complex;
 using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+using System.Linq;
+using System.Diagnostics;
+using Unity.Editor.Tasks;
+using UnityEngine.UI;
 
 public class ZetaSolver : MonoBehaviour
 {
@@ -15,7 +21,7 @@ public class ZetaSolver : MonoBehaviour
 
     public int numOfBands = 3;
     public Vector2 bandSize = new Vector2(0.25f, 1);
-    [Tooltip("Number of dots between")] public Vector2 bandDensity = new Vector2(10, 40);
+    [Tooltip("Number of points between")] public Vector2 bandDensity = new Vector2(10, 40);
     public List<Color> bandColors = new List<Color>() 
     {
         Color.red,
@@ -24,43 +30,63 @@ public class ZetaSolver : MonoBehaviour
         Color.magenta,
     };
     [Range(0, 1)] public float bandTransparency = 1;
-    // public float dotRadiusScalar = 300;
-    [Range(0, 4)] public float indexDotRadius = 2.0f;
-    private struct IndexDot
+    // public float pointRadiusScalar = 300;
+    [Range(0, 4)] public float indexPointRadius = 2.0f;
+    private struct IndexPoint
     {
-        public IndexDot(Vector pPos, Color pColor)
+        public IndexPoint(Vector2 pPos, Color pColor)
         {
             pos = pPos;
             color = pColor;
         }
-        public Vector pos;
+        public Vector2 pos;
         public Color color;
     }
-    private List<IndexDot> _indexDots;
+    private List<IndexPoint> _indexPoints;
 
     [Header("TeardropPlane")]
     public Camera TeardropCamera;
     public Transform teardropPlaneOrigin;
-    [Range(0, 1)] public float teardropDotTransparency = 1;
+    [Range(0, 1)] public float teardropPointTransparency = 1;
     // public float dotRadiusScalar = 300;
-    [Range(0, 4)] public float teardropDotRadius = 2.0f;
-    private struct TeardropDot
+    [Range(0, 4)] public float teardropPointRadius = 2.0f;
+    private struct TeardropPoint
     {
-        public TeardropDot(Vector pPos, IndexDot pPair)
+        public TeardropPoint(Vector2 pPos, IndexPoint pPair)
         {
             pos = pPos;
             pair = pPair;
         }
-        public Vector pos;
-        public IndexDot pair;
+        public Vector2 pos;
+        public IndexPoint pair;
 
         public Color GetColor()
         {
             return pair.color;
         }
     }
-    private List<TeardropDot> _teardropDots;
+    private List<TeardropPoint> _teardropPoints;
     #endregion
+
+    // Job struct to perform the calculations
+    private struct CalculateZetaPoints : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<IndexPoint> IndexPoints;
+
+        [WriteOnly]
+        public NativeArray<TeardropPoint> TeardropPoints;
+
+        public void Execute(int index)
+        {
+            IndexPoint indexPoint = IndexPoints[index];
+
+            Complex s = new Complex(indexPoint.pos.x, Zeta.IndexToImag(indexPoint.pos.y));
+            Vector2 teardropPos = Zeta.TearDrop(0, 1, s).ToVector2();
+
+            TeardropPoints[index] = new TeardropPoint(teardropPos, indexPoint);
+        }
+    }
 
     public void Start()
     {
@@ -75,17 +101,24 @@ public class ZetaSolver : MonoBehaviour
 
     public void OnDrawShapes(Camera cam)
     {
-        DrawIndexPlanePoints();
-        DrawTeardropPlanePoints();
+        if(bandTransparency > 0.1f)
+        {
+            DrawIndexPlanePoints();
+        }
+
+        if(teardropPointTransparency > 0.1f)
+        {
+            DrawTeardropPlanePoints();
+        }
     }
 
     private void CreateIndexPlanePoints()
     {
-        _indexDots = new List<IndexDot>();
+        _indexPoints = new List<IndexPoint>();
         
         for(int band = 0; band < numOfBands; band++)
         {
-            Color drawColor = bandColors[band];
+            Color drawColor = bandColors[band % 4];
             drawColor.a = bandTransparency;
 
             if(bandDensity.x <= 0) bandDensity.x = 0.001f;
@@ -99,7 +132,7 @@ public class ZetaSolver : MonoBehaviour
                     var offset = new Vector(indexPlaneOrigin.position.x + indexPlaneOffset.x, indexPlaneOrigin.position.y + indexPlaneOffset.y);
                     offset += new Vector(bandSize.x * band, 0);
                     var pt = new Vector(x, y) + offset;
-                    _indexDots.Add(new IndexDot(pt, drawColor));
+                    _indexPoints.Add(new IndexPoint(pt, drawColor));
                 }
             }
         }
@@ -107,15 +140,49 @@ public class ZetaSolver : MonoBehaviour
 
     private void CreateTeardropPoints()
     {
-        _teardropDots = new List<TeardropDot>();
+        // _teardropPoints = new List<TeardropPoint>();
 
-        foreach(IndexDot dot in _indexDots)
+        // foreach(IndexPoint point in _indexPoints)
+        // {
+        //     Complex s = new Complex(point.pos.x, Zeta.IndexToImag(point.pos.y));
+        //     // var offset = new Vector(teardropPlaneOrigin.position.x, teardropPlaneOrigin.position.y);
+        //     var pt = Zeta.TearDrop(0, 1, s).ToVector();
+        //     _teardropPoints.Add(new TeardropPoint(pt, point));
+        // }
+
+
+        // Job Attempt
+        _teardropPoints = new List<TeardropPoint>(_indexPoints.Count);
+
+        // Convert indexPoints list to native array for job processing
+        NativeArray<IndexPoint> inputPointsArray = new NativeArray<IndexPoint>(_indexPoints.Count, Allocator.TempJob);
+        for(int i = 0; i < _indexPoints.Count; i++)
         {
-            Complex s = new Complex(dot.pos.x, Zeta.IndexToImag(dot.pos.y));
-            // var offset = new Vector(teardropPlaneOrigin.position.x, teardropPlaneOrigin.position.y);
-            var pt = Zeta.TearDrop(0, 1, s).ToVector();
-            _teardropDots.Add(new TeardropDot(pt, dot));
+            inputPointsArray[i] = _indexPoints[i];
         }
+
+        // Create a native array for the teardropPoints
+        NativeArray<TeardropPoint> teardropPointsArray = new NativeArray<TeardropPoint>(_indexPoints.Count, Allocator.TempJob);
+
+        // Create a job and set its data
+        CalculateZetaPoints pointJob = new CalculateZetaPoints
+        {
+            IndexPoints = inputPointsArray,
+            TeardropPoints = teardropPointsArray
+        };
+
+        // Schedule the job
+        JobHandle jobHandle = pointJob.Schedule(inputPointsArray.Length, 64);
+
+        // Wait for the job to complete
+        jobHandle.Complete();
+
+        // Convert the native array back to a managed array
+        _teardropPoints = teardropPointsArray.ToList();
+
+        // Dispose of the native arrays
+        inputPointsArray.Dispose();
+        teardropPointsArray.Dispose();
     }
 
     private void DrawIndexPlanePoints()
@@ -123,9 +190,9 @@ public class ZetaSolver : MonoBehaviour
         using (Draw.StyleScope)
         {
             Draw.Matrix = indexPlaneOrigin.localToWorldMatrix;
-            foreach(IndexDot dot in _indexDots)
+            foreach(IndexPoint pt in _indexPoints)
             {
-                Draw.Disc(dot.pos, indexDotRadius / 1000, dot.color);
+                Draw.Disc(pt.pos, indexPointRadius / 1000, pt.color);
             }
         }
     }
@@ -135,9 +202,11 @@ public class ZetaSolver : MonoBehaviour
         using (Draw.StyleScope)
         {
             Draw.Matrix = teardropPlaneOrigin.localToWorldMatrix;
-            foreach(TeardropDot dot in _teardropDots)
+            foreach(TeardropPoint pt in _teardropPoints)
             {
-                Draw.Disc(dot.pos, teardropDotRadius / 1000, dot.GetColor());
+                Color drawColor = pt.GetColor();
+                drawColor.a = teardropPointTransparency;
+                Draw.Disc(pt.pos, teardropPointRadius / 1000, drawColor);
             }
         }
     }
