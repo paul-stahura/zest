829a283c3b4009f3d8381d87023fd96fc9057cec
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2024-02-27 01:00:15 -0800

    Family Range and CSVs
    added family range and multiple csv outputs

--- Assets/app/ClockPoints.cs
+++ Assets/app/ClockPoints.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using Shapes;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,14 +10,16 @@ public class ClockPoints : MonoBehaviour
     [Header("Input")]
     [SerializeField] private App app;
     [SerializeField] private FloatInput nPtsInput;
-    [SerializeField] private FloatInput nFamilyInput;
+    [SerializeField] private FloatInput nFamilyInputStart;
+    [SerializeField] private FloatInput nFamilyInputEnd;
     [SerializeField] private Slider clockTransparencySlider;
     [SerializeField] private Button createPointsButton;
     [SerializeField] private Button writePointsButton;
 
     [Header("Settings")]
     [SerializeField] private int nPts = 200;
-    [SerializeField] private int nFamily = 4;
+    [SerializeField] private int nFamilyStart = 0;
+    [SerializeField] private int nFamilyEnd = 4;
     [SerializeField] private Color LineColorA = Color.red;
     [SerializeField] private Color LineColorB = Color.green;
     [SerializeField] private Color LineColorInf = Color.cyan;
@@ -42,7 +45,7 @@ public class ClockPoints : MonoBehaviour
 
         if(writePoints)
         {
-            WritePointTable(_ptTable);
+            WritePointTables(_ptTable);
             writePoints = false;
         }
     }
@@ -59,11 +62,18 @@ public class ClockPoints : MonoBehaviour
             nPts = (int)value;
         });
 
-        nFamilyInput = GameObject.Find("ClockFamily")?.GetComponent<FloatInput>();
-        nFamilyInput.Value = nFamily;
-        nFamilyInput?.onValueChanged.AddListener((float value) =>
+        nFamilyInputStart = GameObject.Find("ClockFamilyStart")?.GetComponent<FloatInput>();
+        nFamilyInputStart.Value = nFamilyStart;
+        nFamilyInputStart?.onValueChanged.AddListener((float value) =>
         {
-            nFamily = (int)value;
+            nFamilyStart = (int)value;
+        });
+
+        nFamilyInputEnd = GameObject.Find("ClockFamilyEnd")?.GetComponent<FloatInput>();
+        nFamilyInputEnd.Value = nFamilyEnd;
+        nFamilyInputEnd?.onValueChanged.AddListener((float value) =>
+        {
+            nFamilyEnd = (int)value;
         });
 
         clockTransparencySlider = GameObject.Find("ClockTransparencySlider")?.GetComponent<Slider>();
@@ -82,7 +92,8 @@ public class ClockPoints : MonoBehaviour
         writePointsButton = GameObject.Find("WriteClockPts")?.GetComponent<Button>();
         writePointsButton?.onClick.AddListener(() =>
         {
-            WritePointTable(_ptTable);
+            CreatePointTable();
+            WritePointTables(_ptTable);
         });
 
     }
@@ -103,6 +114,12 @@ public class ClockPoints : MonoBehaviour
                 Vector start = _ptTable[i][0];
                 for(int k = 1; k < _ptTable[i].Length; k++)
                 {
+                    if(k % nPts == 0) 
+                    {
+                        start = _ptTable[i][k];
+                        continue;
+                    }
+
                     Vector end = _ptTable[i][k];
 
                     Draw.Color = i % 2 == 0 ? LineColorA : LineColorB;
@@ -122,44 +139,57 @@ public class ClockPoints : MonoBehaviour
 
     public void CreatePointTable()
     {
+        if(nFamilyEnd < nFamilyInputStart) return;
+
         _ptTable = new Vector[6][];
         for(int i = 0; i < 6; i++)
         {
-            _ptTable[i] = new Vector[nPts];
+            _ptTable[i] = new Vector[nPts * (nFamilyEnd + 1 - nFamilyStart)];
         }
 
-        for(int i = 0; i < nPts; i++)
+        for(int n = 0; n <= nFamilyEnd - nFamilyStart; n++)
         {
-            double index = D(nFamily, nPts, i);
+            var nFamily = n + nFamilyStart;
 
-            Vector z = new Vector(0, 0);
-            _ptTable[0][i] = z;
-            _ptTable[1][i] = z;
-            _ptTable[2][i] = z;
-            _ptTable[3][i] = z;
-            _ptTable[4][i] = z;
-            _ptTable[5][i] = z;
+            for(int i = 0; i < nPts; i++)
+            {
+                double index = D(nFamily, nPts, i);
 
+                int tableIndex = i + n*nPts;
 
-            // arms
-            _ptTable[0][i] = GetRedArm(index);
-            // Debug.Log($"{index}, ArmR({_ptTable[0][i]})");
-            _ptTable[1][i] = GetGreenArm(index);
+                // arms
+                _ptTable[0][tableIndex] = GetRedArm(index);
+                // Debug.Log($"{index}, ArmR({_ptTable[0][i]})");
+                _ptTable[1][tableIndex] = GetGreenArm(index);
 
-            // tdrops
-            _ptTable[2][i] = Zeta.InfinityTdrop(index - nFamily, true) + new Vector(1, 0);
-            _ptTable[3][i] = Zeta.InfinityTdrop(index - nFamily, false);
+                // tdrops
+                _ptTable[2][tableIndex] = Zeta.InfinityTdrop(index - nFamily, true) + new Vector(1, 0);
+                _ptTable[3][tableIndex] = Zeta.InfinityTdrop(index - nFamily, false);
 
-            double imag = Zeta.IndexToImag(index);
-            _ptTable[4][i] = Zeta.TearDrop(nFamily + 1, 0.5, imag);
-            _ptTable[5][i] = Zeta.TearDrop(nFamily + 1, 0.5, imag, true);
+                double imag = Zeta.IndexToImag(index);
+                _ptTable[4][tableIndex] = Zeta.TearDrop(nFamily + 1, 0.5, imag);
+                _ptTable[5][tableIndex] = Zeta.TearDrop(nFamily + 1, 0.5, imag, true);
+            }
         }
+        
     }
 
-    public void WritePointTable(Vector[][] table)
+    public void WritePointTables(Vector[][] table)
+    {
+        WriteFullPointTable(table);
+        WriteFamilyPointTables(table);
+        WriteIndividualPointTables(table);
+
+        // Refresh the Unity editor to reflect changes
+        UnityEditor.AssetDatabase.Refresh();
+
+        Application.OpenURL(Application.dataPath + "/StreamingAssets");
+    }
+
+    private void WriteFullPointTable(Vector[][] table)
     {
         // write table
-        string fileName = "ClockArmPoints.csv";
+        string fileName = "FullClockArmPoints.csv";
 
         // Combine the path to the "Resources" folder with the file name
         string filePath = Path.Combine(Application.dataPath + "/StreamingAssets", fileName);
@@ -168,12 +198,13 @@ public class ClockPoints : MonoBehaviour
         using (StreamWriter writer = new StreamWriter(filePath, false))
         {
             // header
-            writer.WriteLine($"index, Ar.x, Ar.y, Ag.x, Ag.y, InfA.x, InfA.y, InfB.x, InfB.y, Tr.x, Tr.y, Tg.x, Tg.y");
+            writer.WriteLine($"index, Ar.x, Ar.y, Ag.x, Ag.y, InfR.x, InfR.y, InfG.x, InfG.y, Tr.x, Tr.y, Tg.x, Tg.y");
 
             // points
             for(int i = 0; i < table[0].Length; i++)
             {
-                writer.Write($"{D(nFamily, nPts, i)}");
+                int fam = nFamilyStart + (i / nPts);
+                writer.Write($"{D(fam, nPts, i % nPts)}");
                 for(int j = 0; j < table.Length; j++)
                 {
                     writer.Write($", {table[j][i]}");
@@ -181,11 +212,75 @@ public class ClockPoints : MonoBehaviour
                 writer.WriteLine("");
             }
         }
+    }
 
-        // Refresh the Unity editor to reflect changes
-        UnityEditor.AssetDatabase.Refresh();
+    private void WriteFamilyPointTables(Vector[][] table)
+    {
+        int famCount = nFamilyEnd - nFamilyStart + 1;
+        for(int f = 0; f < famCount; f++)
+        {
+            // write table
+            string fileName = $"ClockArmPointsFam{nFamilyStart + f}.csv";
 
-        Application.OpenURL(Application.dataPath + "/StreamingAssets");
+            // Combine the path to the "Resources" folder with the file name
+            string filePath = Path.Combine(Application.dataPath + "/StreamingAssets", fileName);
+
+            // Create or overwrite the file
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                // header
+                writer.WriteLine($"index, Ar.x, Ar.y, Ag.x, Ag.y, InfR.x, InfR.y, InfG.x, InfG.y, Tr.x, Tr.y, Tg.x, Tg.y");
+
+                // points
+                for(int i = 0; i < nPts; i++)
+                {
+                    int index = i + f*nPts;
+                    int fam = nFamilyStart + f;
+                    writer.Write($"{D(fam, nPts, i)}");
+                    for(int j = 0; j < table.Length; j++)
+                    {
+                        writer.Write($", {table[j][index]}");
+                    }
+                    writer.WriteLine("");
+                }
+            }
+        }
+    }
+
+    private void WriteIndividualPointTables(Vector[][] table)
+    {
+        string[] fileNames = new string[] {"ArPoints.csv", "AgPoints.csv", "InfRPoints.csv", "InfGPoints.csv", "TrPoints.csv", "TgPoints.csv"};
+        string[] fileHeaders = new string[] {"Ar", "Ag", "InfR", "InfG", "Tr", "Tg"};
+        int famCount = nFamilyEnd - nFamilyStart + 1;
+
+        for(int f = 0; f < fileNames.Length; f++)
+        {
+            string fileName = fileNames[f];
+            string filePath = Path.Combine(Application.dataPath + "/StreamingAssets", fileName);
+
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                // header
+                writer.Write("index");
+                for(int i = 0; i < famCount; i++)
+                {
+                    writer.Write($", F{nFamilyStart + i} {fileHeaders[f]}.x, F{nFamilyStart + i} {fileHeaders[f]}.y");
+                }
+                writer.WriteLine("");
+
+
+                // points
+                for(int i = 0; i < nPts; i++)
+                {
+                    writer.Write($"{D(0, nPts, i)}");
+                    for(int j = 0; j < famCount; j++)
+                    {
+                        writer.Write($", {table[f][i + j*nPts]}");
+                    }
+                    writer.WriteLine("");
+                }
+            }
+        }
     }
 
     private Vector GetRedArm(double t)
