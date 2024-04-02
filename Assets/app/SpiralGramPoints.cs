ec0c65b0c30cee072237a12adf4f0099d831c417
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2024-04-01 17:49:58 -0700

    Gram Points slider

--- Assets/app/SpiralGramPoints.cs
+++ Assets/app/SpiralGramPoints.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +5,10 @@ using UnityEngine.UI;
 public class SpiralGramPoints : MonoBehaviour
 {
     [SerializeField] private App _app;
-    [SerializeField] private FloatInput _gramPointInput;
+    [SerializeField] private Slider _gramPointInputSlider;
+    [SerializeField] private TMP_Text _gramPointInputText;
+    [SerializeField] private FloatInput _gramPointInputMin;
+    [SerializeField] private FloatInput _gramPointInputMax;
     private GramPoints _gramPoints;
 
     void Awake()
@@ -17,11 +17,31 @@ public class SpiralGramPoints : MonoBehaviour
 
         _app = GameObject.Find("App")?.GetComponent<App>();
 
-        _gramPointInput = GameObject.Find("GramPointInput")?.GetComponent<FloatInput>();
-        _gramPointInput.onValueChanged.AddListener((value) =>
+        _gramPointInputText = GameObject.Find("GramPointInputText")?.GetComponent<TMP_Text>();
+
+
+        _gramPointInputSlider = GameObject.Find("GramPointInputSlider")?.GetComponent<Slider>();
+        _gramPointInputSlider.onValueChanged.AddListener((value) =>
         {
-            LoadGramPoint((int)value);
+            int pt = (int)value;
+            LoadGramPoint(pt);
+            _gramPointInputText.SetText(pt.ToString());
         });
+
+        _gramPointInputMin = GameObject.Find("GramPointInputMIN")?.GetComponent<FloatInput>();
+        _gramPointInputMin.onValueChanged.AddListener((value) =>
+        {
+            _gramPointInputSlider.minValue = (int)value;
+        });
+
+        _gramPointInputMax = GameObject.Find("GramPointInputMAX")?.GetComponent<FloatInput>();
+        _gramPointInputMax.onValueChanged.AddListener((value) =>
+        {
+            _gramPointInputSlider.maxValue = (int)value;
+        });
+
+        _gramPointInputSlider.minValue = _gramPointInputMin.Value;
+        _gramPointInputSlider.maxValue = _gramPointInputMax.Value;
     }
 
     public void LoadGramPoint(int index)
