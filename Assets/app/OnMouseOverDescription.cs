82eafdfc803abb49ca1fb8ba78a71981bda20901
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2024-04-16 15:52:47 -0700

    removes description on exit

--- Assets/app/OnMouseOverDescription.cs
+++ Assets/app/OnMouseOverDescription.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class OnMouseOverDescription: MonoBehaviour, IPointerEnterHandler{
+public class OnMouseOverDescription: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
 	
     public string description = "Tell me more";
     public void OnPointerEnter(PointerEventData eventData)
     {
         Manual.UpdateDescription(description);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Manual.UpdateDescription("");
     }
 }
