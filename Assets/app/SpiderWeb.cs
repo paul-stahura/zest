bd273fc582bbb95c87a2395b3e33912070aa8775
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2024-07-30 21:51:06 -0700

    Drawing Web

--- Assets/app/SpiderWeb.cs
+++ Assets/app/SpiderWeb.cs
@@ -6,13 +6,23 @@ using UnityEngine;
 
 public class SpiderWeb : MonoBehaviour
 {
-    [SerializeField] private float _lineTransparency = 0.8f;
+    [Header("Unit Points")]
     [SerializeField] private bool _drawUpToMiddlePlusOne = true;
     [SerializeField] private int _unitPointsToDraw = 10;
+
+    [Header("Web")]
+    [SerializeField] private int _pointsPerWeb = 100;
+    [SerializeField] private int _webIndex = -1;
+    [SerializeField] private int _webCount = 1;
+    [SerializeField] private List<Vector> _webPoints;
+
+    [Header("Draw")]
+    [SerializeField] private float _lineTransparency = 0.8f;
     [SerializeField] private Color _circleColor = Color.white;
     [SerializeField] private Color _pointColor = Color.green;
-    [SerializeField] private float _pointSize = 0.1f;
+    [SerializeField] private float _pointSize = 0.04f;
     [SerializeField] private Color _webColor = Color.red;
+
 
     private App _app;
 
@@ -26,6 +36,7 @@ public class SpiderWeb : MonoBehaviour
     
     private void DrawSpiral(Camera cam, Zeta.Spiral s)
     {
+        // unit circle
         using(Draw.StyleScope)
         {
             _pointColor.a = _lineTransparency;
@@ -36,33 +47,25 @@ public class SpiderWeb : MonoBehaviour
             Draw.Ring(Vector2.zero, 1);
         }
 
-
+        // points to draw
         if(_drawUpToMiddlePlusOne)
         {
             _unitPointsToDraw = s.middleIndex + 1;
         }
 
-        // calculate the angle of each joint
-        List<double> angles = new List<double>();
-        // joint zero is (0,0), so we start at 1 and 2
-        for(int i = 2; i <= _unitPointsToDraw; i++)
-        {
-            if(i > s.joints.Count()) break;
-            
-            Vector last = s.joints[i-1];
-            Vector a = s.joints[i] - last;
-            angles.Add(GetAngle(a, last) + (Math.PI / 2));
+        DrawUnitPoints(s);
 
-            // the first angle is right, something is off on the following ones
-            Debug.Log(GetAngle(a, last) * Mathf.Rad2Deg);
+        int newIndex = (int)Mathf.Floor((float)s.index);
+        if(_webIndex != newIndex)
+        {
+            _webIndex = newIndex;
+            CalcWebPoints(s);
         }
 
-        DrawUnitPoints(angles);
-
-
+        DrawWeb();
     }
 
-    private void DrawUnitPoints(List<double> angles)
+    private void DrawUnitPoints(Zeta.Spiral s)
     {
         using(Draw.StyleScope)
         {
@@ -71,31 +74,100 @@ public class SpiderWeb : MonoBehaviour
             Draw.Thickness = 1 + _lineTransparency;
 
             // first angle is always zero with length of 1
-            Draw.Ring(Vector2.right, _pointSize);
-
-            for(int i = 0; i < angles.Count; i++)
+            Vector pos = new Vector(1.0, 0.0);
+            for(int i = 0; i < _unitPointsToDraw; i++)
             {
-                Vector2 pos = new Vector(Math.Sin(angles[i]), Math.Cos(angles[i]));
+                // the angle is relitive to the previous point, so we can just rotate based off our last position
+                pos = Rotate2DPoint(pos, GetJointAngle(i, s));
                 Draw.Ring(pos, _pointSize);
             }
         }
     }
 
+    private void CalcWebPoints(Zeta.Spiral s)
+    {
+        List<double> dists = new List<double>();
 
-    private void DrawWeb(Zeta.Spiral s)
+        double inc = 1.0d / _pointsPerWeb;
+        double index = _webIndex;
+        Zeta.Spiral webSpiral = new Zeta.Spiral(s.real, index, SpiralFormulas.EulerMaclauren);
+
+        while(index < _webIndex + _webCount)
+        {
+            // get unitPoint
+            Vector pos = new Vector(1.0, 0.0);
+            for(int i = 0; i < _unitPointsToDraw; i++)
+            {
+                // the angle is relitive to the previous point, so we can just rotate based off our last position
+                pos = Rotate2DPoint(pos, GetJointAngle(i, webSpiral));
+            }
+
+            // save dist from unit point to zeta
+            dists.Add(Vector.Distance(pos, webSpiral.zeta.ToVector()));
+            
+            index += inc;
+            webSpiral.Update(webSpiral.real, index, SpiralFormulas.EulerMaclauren);
+        }
+
+        _webPoints = new List<Vector>();
+
+        // IDEA/TODO: play with a different way to distribute the points around the circle
+        double angleInc = Math.PI * 2 / _pointsPerWeb;
+        double angle = 0;
+        _webPoints.Add(new Vector(1.0, 0.0) * dists[0]);
+        for(int i = 1; i < dists.Count; i++)
+        {
+            // reset vector, add angle
+            Vector pos = new Vector(1.0, 0.0);
+            angle += angleInc;
+            _webPoints.Add(Rotate2DPoint(pos, angle) * dists[i]);
+        }
+    }
+    private void DrawWeb()
     {
         using(Draw.StyleScope)
         {
+            _webColor.a = _lineTransparency;
+            Draw.Color = _webColor;
             Draw.Thickness = 1 + _lineTransparency;
 
-
+            for(int i = 1; i < _webPoints.Count(); i++)
+            {
+                Draw.Line(_webPoints[i-1], _webPoints[i]);
+            }
         }
     }
 
-    private double GetAngle(Vector2 a, Vector2 b)
+    private double GetJointAngle(int jointIndex, Zeta.Spiral s)
     {
-        double aAngle = Math.Atan2(a.y, a.x);
-        double bAngle = Math.Atan2(b.y, b.x);
-        return bAngle - aAngle;
+        if(jointIndex <= 0 || jointIndex > s.joints.Count()) return 0;
+
+        Vector link1 = s.joints[jointIndex] - s.joints[jointIndex-1];
+        Vector link2 = s.joints[jointIndex + 1] - s.joints[jointIndex];
+        return GetAngle(link2, link1);
+    }
+
+    private double GetAngle(Vector a, Vector b)
+    {
+        double dotProduct = a.Dot(b);
+        double magnitudeProduct = a.Length * b.Length;
+        double angle = Math.Acos(dotProduct / magnitudeProduct);
+
+        // Check if the vectors are in the same or opposite direction
+        // > 0 = clockwise
+        if (Vector3.Cross(a, b).z > 0)
+        {
+            angle = 2 * Math.PI - angle;
+        }
+
+        return angle;
+    }
+
+    private Vector Rotate2DPoint(Vector point2D, double rad)
+    {
+        double rotatedX = point2D.x * Math.Cos(rad) - point2D.y * Math.Sin(rad);
+        double rotatedY = point2D.x * Math.Sin(rad) + point2D.y * Math.Cos(rad);
+
+        return new Vector(rotatedX, rotatedY);
     }
 }
