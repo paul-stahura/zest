5ee83882fd951e1d7a52a5dbe6e3f09ece3059f6
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2024-11-26 11:54:26 -0800

    real path 1-infinity
    added change in color

--- Assets/app/ZetaSpiral.cs
+++ Assets/app/ZetaSpiral.cs
@@ -279,21 +279,37 @@ public partial class ZetaSpiral : MonoBehaviour
         {
             using(Draw.StyleScope)
             {
-                var pathColor = Color.magenta;
+                var pathColor1 = Color.magenta;
+                pathColor1.a = targetTransparency.value;
+
+                var pathColor = Color.blue;
                 pathColor.a = targetTransparency.value;
+
                 Draw.Thickness = 1;
-                Draw.Color = pathColor;
+                Draw.Color = pathColor1;
 
                 int ptCount = 100;
                 Zeta.Spiral rspiral = new Zeta.Spiral(0, s.index, (SpiralFormulas)_spiralFormula.value, app.useNewImagToggle.isOn);
                 pt = rspiral.zeta.ToVector2();
-                for(int i = 1; i <= ptCount; i++)
+                // from real 0-6
+                for(int i = 0; i <= 10; i++)
                 {
-                    float r = (float)i/ptCount;
-                    rspiral = new Zeta.Spiral(r, s.index, (SpiralFormulas)_spiralFormula.value, app.useNewImagToggle.isOn);
-                    Vector2 nextTarget = rspiral.zeta.ToVector2();
-                    Draw.Line(pt, nextTarget);
-                    pt = nextTarget;
+                    if(i > 0) Draw.Color = pathColor;
+                    
+                    ptCount = 100 / (i + 1);
+                    for(int j = 1; j <= ptCount; j++)
+                    {
+                        float r = (float)j/ptCount + i;
+                        rspiral = new Zeta.Spiral(r, s.index, (SpiralFormulas)_spiralFormula.value, app.useNewImagToggle.isOn);
+                        Vector2 nextTarget = rspiral.zeta.ToVector2();
+                        Draw.Line(pt, nextTarget);
+                        pt = nextTarget;
+                    }
+
+                    if(i == 10)
+                    {
+                        Draw.Line(pt, new Vector2(1, 0));
+                    }
                 }
 
                 // Draw.Line(new Zeta.Spiral(0, s.index, (SpiralFormulas)_spiralFormula.value, app.useNewImagToggle.isOn).zeta.ToVector2(), pt);
