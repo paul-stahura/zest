d5fecf6ad32b4e2ea1fa33ee10267be8826f2a6c
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2025-04-15 13:27:29 -0700

    new data

--- Assets/app/critical-strip/Editor/FindIntersections.cs
+++ Assets/app/critical-strip/Editor/FindIntersections.cs
@@ -125,20 +125,20 @@ public static class FindIntersections
         var twoPI = 2.0*Math.PI;
 
         
-        // // IR intersection angle
-        // var r = 0.25;
-        // // double ThetaTwo = GetForwardRhombusAngle(((int)r == 2) ? 0.5 : (int)r, index);
-        // double ThetaTwo = GetForwardRhombusAngle(r, index);
-        // // transform to fit BisectorPoint ThetaAngle
-        // if(ThetaTwo < 0)
-        //     ThetaTwo += 360;
+        // IR intersection angle
+        var r = 0.375;
+        // double ThetaTwo = GetForwardRhombusAngle(((int)r == 2) ? 0.5 : (int)r, index);
+        double ThetaTwo = GetForwardRhombusAngle(r, index);
+        // transform to fit BisectorPoint ThetaAngle
+        if(ThetaTwo < 0)
+            ThetaTwo += 360;
 
-        // // Convert to radians
-        // ThetaTwo = ThetaTwo * Math.PI / 180.0;
+        // Convert to radians
+        ThetaTwo = ThetaTwo * Math.PI / 180.0;
 
 
-        // theta formula
-        var ThetaTwo = -BisectorPoint.ThetaTwo(index);
+        // // theta formula
+        // var ThetaTwo = -BisectorPoint.ThetaTwo(index);
 
         var phase = (ThetaTwo % twoPI) / twoPI;
         var pt = new Vector(phase, index);
