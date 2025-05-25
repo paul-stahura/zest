8c3bc50f745762d9b4842a0ff6504ced2b42bc70
Fish1naTank <58810573+Fish1naTank@users.noreply.github.com>
2025-05-24 19:35:59 -0700

    132 zps general from remainder middle point (#133)
    * Bisectors using remainder formula
    
    * Rhombus paths now use general bisector points

--- Assets/app/RhombusPoints.cs
+++ Assets/app/RhombusPoints.cs
@@ -5,17 +5,21 @@ public class RhombusPoints : MonoBehaviour
     // the intersection of bpForward and bpSymmetry = rhombus
     public static Vector2 GetBPForward(double real, double index)
     {
-        (Vector2 p1, Vector2 p2) = BisectorPoint.BisectorLink(real, index);
-        var link = p2 - p1;
-        return p1 + link * (float)BisectorPoint.Djoint(index);
+        // (Vector2 p1, Vector2 p2) = BisectorPoint.BisectorLink(real, index);
+        // var link = p2 - p1;
+        // return p1 + link * (float)BisectorPoint.Djoint(index);
+
+        return ZpsGeneral.ForwardBisector(real, index);
     }
 
     // when bpForward and bpInverse are opposites (bpF.x == -bpI.x && bpF.y == -bpI.y), we have a zero!
     public static Vector2 GetBPInverse(double real, double index)
     {
-        (Vector2 p1, Vector2 p2) = NewRiemmanSeigalFormulaSums.InverseBisectorLink(real, index);
-        var link = p2 - p1;
-        return p1 + link * (float)BisectorPoint.Djoint(index);
+        // (Vector2 p1, Vector2 p2) = NewRiemmanSeigalFormulaSums.InverseBisectorLink(real, index);
+        // var link = p2 - p1;
+        // return p1 + link * (float)BisectorPoint.Djoint(index);
+
+        return ZpsGeneral.InverseBisector(real, index);
     }
 
     // intersection of foraward and inverse reflected bisector links
